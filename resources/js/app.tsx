a9d4f2e6b9ff39bea7824232e83640374065d202
mustaham <mustahammicah@gmail.com>
2025-03-12 16:27:55 +0800

    fixed bugs

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,13 +3,13 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 import { route as routeFn } from 'ziggy-js';
-import { initializeTheme } from './hooks/use-appearance';
+import { initializeTheme } from '@/hooks/use-appearance';
 import React from 'react';
-import { LazyPages } from './utils/lazy-inertia';
+import { LazyPages } from '@/utils/lazy-inertia';
 
 declare global {
     const route: typeof routeFn;
-}
+}   
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
